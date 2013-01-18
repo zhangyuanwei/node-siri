d3678d8cc9ae28a7691621aea3e8749becc70ac8
zhangyuanwei <zhangyuanwei1988@gmail.com>
2013-01-18 09:47:00 +0800

    完善检测界面

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 node-siri
 =========
 
-nodejs Siri代理.
+* nodejs Siri代理.
 
-1. 想办法将 guzzoni.apple.com 解析到运行代理的机器
-2. 运行 `node install.js`
-3. 通过IOS设备访问 http://guzzoni.apple.com，如果能看到欢迎界面，就离成功不远了
-4. 点击欢迎页上的链接安装证书
-5. 运行测试程序 `node test.js`
-6. 在Siri里说“你好”，代理会回复“Siri代理向你问好!”
-7. 写自己的程序，尽情的调戏Siri吧。
+1. 想办法将 guzzoni.apple.com DNS解析到运行代理的机器.
+2. 运行 `sudo node install.js` ,install.js使用80和443端口，所以需要root权限.
+3. 通过IOS设备访问 http://guzzoni.apple.com ，系统会自动检测配置.
+4. 写自己的程序，尽情的调戏Siri吧。
+
+* 示例
 
 ``` javascript
 var fs = require("fs"),
 	siri = require("./siri");
 
 siri.createServer({
-	key: fs.readFileSync('./server.passless.key'),
-	cert: fs.readFileSync('./server.passless.crt')
+	key: fs.readFileSync('./server-key.pem'),
+	cert: fs.readFileSync('./server-cert.pem')
 }, function(command, device) {
 	if (command == "你好") {
 		device.end("Siri代理向你问好!");
@@ -30,25 +29,5 @@ siri.createServer({
 
 ```
 
-支持回调
-``` javascript
-var fs = require("fs"),
-	siri = require("./siri");
-
-siri.createServer({
-	key: fs.readFileSync('./server.passless.key'),
-	cert: fs.readFileSync('./server.passless.crt')
-}, function(command, device) {
-	if (command == "洗衣服") {
-		device.say("正在为你洗衣服");
-		//为你洗衣服、脱水、晾衣服^_^...
-		setTimeout(function() {
-			device.end("衣服洗好了，主人.");
-		}, 1000);
-	} else {
-		device.proxy();
-	}
-}).listen(443, function() {
-	console.log("Proxy start.");
-});
-```
+* API
+待完善
--- install.js
+++ install.js
@@ -1,17 +1,113 @@
 var http = require("http"),
-    fs = require("fs");
+    https = require("https"),
+    fs = require("fs"),
+    siri = require("./siri");
+
+var pingImg = new Buffer("R0lGODlhAQABAJH/AP///wAAAMDAwAAAACH5BAEAAAIALAAAAAABAAEAAAICVAEAOw==", "base64"),
+    httpsServer = null,
+    siriServer = null;
+
+function pong(res) {
+    res.setHeader("Connection", "close")
+    res.setHeader("Content-Type", "image/gif");
+    res.end(pingImg);
+}
+
+function startHttpsServer(callback) {
+    if (siriServer) {
+        console.log("Closing Siri Server...");
+        siriServer.close(function() {
+            siriServer = null;
+            startHttpsServer(callback);
+            console.log("Siri Server closed.");
+        });
+        return;
+    }
+
+    if (!httpsServer) {
+        httpsServer = https.createServer({
+            key: fs.readFileSync('./server-key.pem'),
+            cert: fs.readFileSync('./server-cert.pem')
+        }, function(req, res) {
+            pong(res);
+        });
+        console.log("Starting HTTPS Server...");
+        httpsServer.listen(443, callback);
+    } else {
+        callback();
+    }
+}
+
+
+function startSiriServer(callback) {
+    if (httpsServer) {
+        console.log("Closing HTTPS Server...");
+        httpsServer.close(function() {
+            httpsServer = null;
+            startSiriServer(callback);
+            console.log("HTTPS Server closed.");
+        });
+        return;
+    }
+
+    if (!siriServer) {
+        siriServer = siri.createServer({
+            key: fs.readFileSync('./server-key.pem'),
+            cert: fs.readFileSync('./server-cert.pem')
+        }, function(command, device) {
+            console.log(command);
+            if (command == "你好") {
+                device.say("3");
+                setTimeout(function() {
+                    device.say("2");
+                    setTimeout(function() {
+                        device.say("1");
+                        setTimeout(function() {
+                            device.end("Siri代理向你问好!");
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            } else if (command == "关机") {
+                device.end("正在关机...");
+                require('child_process').exec("sudo halt");
+            } else {
+                device.proxy();
+            }
+        });
+
+        console.log("Starting Siri Server...");
+        siriServer.listen(443, callback);
+    } else {
+        callback();
+    }
+}
 
 http.createServer(function(req, res) {
-    if (/^\/(\?.*)?$/.test(req.url)) {
+    if (/^\/welcome(\?.*)?$/.test(req.url)) {
         res.setHeader("Content-Type", "text/html");
-        res.end(fs.readFileSync("./index.html"));
-    } else if (/^\/ca.pem(\?.*)?$/.test(req.url)) {
+        res.end(fs.readFileSync("./welcome.html"));
+    } else if (/^\/ping.gif(\?.*)?$/.test(req.url)) {
+        pong(res);
+    } else if (/^\/https.gif(\?.*)?$/.test(req.url)) {
+        startHttpsServer(function() {
+            console.log("HTTPS Server start.");
+            pong(res);
+        });
+    } else if (/^\/siri.gif(\?.*)?$/.test(req.url)) {
+        startSiriServer(function() {
+            console.log("Siri Server start.");
+            pong(res);
+        });
+    } else if (/^\/ca(\?.*)?$/.test(req.url)) {
         res.setHeader("Content-Type", "application/x-x509-ca-cert");
         res.end(fs.readFileSync("./server-cert.pem"));
     } else {
         res.writeHead(301, {
-            "Location": "/"
+            "Location": "/welcome"
         });
         res.end("301 Moved Permanently");
     }
-}).listen(8080);
+}).listen(80, function() {
+    console.log("HTTP Server start.");
+});
+console.log("Starting HTTP Server...");
