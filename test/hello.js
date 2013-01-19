75f550252d0ebd7e4d535f321ef002ee305176c2
zhangyuanwei <zhangyuanwei1988@gmail.com>
2013-01-19 12:22:45 +0800

    增加示例

---
+++ test/hello.js
@@ -0,0 +1,9 @@
+var siri = require("../siri");
+
+siri.createServer(function(cmd, dev) {
+    if (/你好/.test(cmd)) {
+        dev.end("Siri代理向你问好.");
+    } else {
+        dev.proxy();
+    }
+}).start();
