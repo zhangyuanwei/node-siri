6d377945b13d4fdb63cfd836e5a5614ab643973e
zhangyuanwei <zhangyuanwei1988@gmail.com>
2013-01-19 12:17:04 +0800

    增加示例

---
+++ test/ask.js
@@ -0,0 +1,16 @@
+var siri = require("../siri");
+
+siri.createServer(function(cmd, dev) {
+    if (/(关机)|(关(闭)?电脑)/.test(cmd)) {
+        dev.ask("确认关机？", function(answer) {
+            if (/(确认)|(是)/.test(answer)) {
+                dev.end("正在关机...");
+                require('child_process').exec("halt");
+            } else {
+                dev.end("好的，取消了。");
+            }
+        });
+    } else {
+        dev.proxy();
+    }
+}).start();
---
+++ test/halt.js
@@ -0,0 +1,10 @@
+var siri = require("../siri");
+
+siri.createServer(function(cmd, dev) {
+    if (/(关机)|(关(闭)?电脑)/.test(cmd)) {
+        dev.end("正在关机...");
+        require('child_process').exec("halt");
+    } else {
+        dev.proxy();
+    }
+}).start();
