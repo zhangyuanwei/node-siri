9bbfb1ee0958b052d3cad460e103d71418e1dc7d
Justin Novack <jnovack@gmail.com>
2013-08-27 13:44:31 -0400

    Add en-US examples

---
+++ examples/en-US/hello.js
@@ -0,0 +1,9 @@
+var siri = require("../siri");
+
+siri.createServer(function(cmd, dev) {
+    if (/Hello/.test(cmd)) {
+        dev.end("Siri Proxy says Hello!");
+    } else {
+        dev.proxy();
+    }
+}).start();
